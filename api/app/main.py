d45c53a42b8460d4ad8afe55df6cc3d9ef69d2f2
HungryArie <arnoudzwakenberg@gmail.com>
2024-01-07 10:51:33 +0000

    add connection to the ws from bybit to relay the orders

--- api/app/main.py
+++ api/app/main.py
@@ -4,6 +4,8 @@ import redis
 from fastapi import FastAPI, WebSocket, Request
 from fastapi.responses import HTMLResponse
 from pybit.unified_trading import HTTP
+import websockets
+
 
 db = redis.Redis(host=os.environ.get("REDIS_HOST"), port=os.environ.get("REDIS_PORT"))
 
@@ -58,13 +60,43 @@ def get_connectioninfo(request: Request):
     return {"client_host": client_host, 'server_adress':server}
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    # https://fastapi.tiangolo.com/advanced/websockets/
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+@app.websocket("/ws_trades")
+async def websocket_endpoint3(websocket: WebSocket):
+    # based on https://fastapi.tiangolo.com/advanced/websockets/
+    # and https://websockets.readthedocs.io/en/stable/reference/asyncio/client.html
+    
+    # accept the websocket request from the user
+    await websocket.accept() 
+
+    # connect to the bybit ws stream
+    uri = "wss://stream.bybit.com/v5/public/linear"
+    # uri = "wss://stream-testnet.bybit.com/v5/public/linear"
+    async with websockets.connect(uri) as websocket_exchange:
+        # suscribe to a stream/channel
+        payload = {
+                "op": "subscribe",
+                "args": [
+                    # "orderbook.1.BTCUSDT",
+                    "publicTrade.BTCUSDT",
+                    # "orderbook.1.ETHUSDT"
+                ]
+            }
+        await websocket_exchange.send(json.dumps(payload))
+
+        # read response of the subscription to the exchange
+        msg = await websocket_exchange.recv()
+
+        # send to 'our' internal websocket clients
+        await websocket.send_text(f"initial message was: {msg}")
+
+        # once connected to the exchanges trade stream, fetch the messages and do something with it
+        while True:
+            # TODO send a ping one every minute or so (https://websockets.readthedocs.io/en/stable/reference/asyncio/client.html#websockets.client.WebSocketClientProtocol.ping)
+            msg_q = websocket_exchange.messages
+            msg = await websocket_exchange.recv()
+            obj = json.loads(msg)
+            print(f"queue length: {len(msg_q)}. data length:{len(obj['data'])}")
+            await websocket.send_text(f"Message data was: {obj['data']}")
 
 
 # use double {{ }} to escape the string formatting =https://stackoverflow.com/questions/5466451/how-do-i-escape-curly-brace-characters-in-a-string-while-using-format-or
@@ -72,31 +104,22 @@ html = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Chat</title>
+        <title>trades</title>
     </head>
     <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
+        <h1>WebSocket live trades</h1>
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket('ws://{}/ws');
+            var ws = new WebSocket('ws://{}/ws_trades');
             ws.onmessage = function(event) {{
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
                 var content = document.createTextNode(event.data)
                 message.appendChild(content)
                 messages.appendChild(message)
+                console.log(message)
             }};
-            function sendMessage(event) {{
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }}
         </script>
     </body>
 </html>
@@ -107,5 +130,3 @@ html = """
 async def ws_test(request: Request):
     con_info = get_connectioninfo(request)
     return HTMLResponse(html.format(con_info["server_adress"]))
-
-
