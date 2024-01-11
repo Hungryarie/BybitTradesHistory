23e0fabafa60cc41ba64261c3ae3a757ec6b9b65
HungryArie <arnoudzwakenberg@gmail.com>
2024-01-11 06:08:08 +0000

    add startup task (backgroundtask) to fetch trades from exchanges websocket: - add logger to replace print statements - add `redis_conn_manager` for backgrountask - add `lifespan` to startup tasks in background - add `/start_trades` to manually add a stream via API

--- api/app/main.py
+++ api/app/main.py
@@ -1,19 +1,49 @@
+import asyncio
 from datetime import datetime
 import json
 import os
+from typing import Annotated
+from redis import ResponseError
 import redis.asyncio as redis
-from fastapi import FastAPI, WebSocket, Request
+from fastapi import FastAPI, WebSocket, Request, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi import Depends
+
 from pybit.unified_trading import HTTP
 import websockets
+from contextlib import asynccontextmanager
+import logging
+
+logger = logging.getLogger(__name__)
+hdlr = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
 
 
-# Dependency to create and manage the Redis connection
+
+
 async def get_redis_conn() -> redis.Redis: 
+    """Dependency for endpoints to create and manage the Redis connection"""
     redis_conn = redis.Redis(host=os.environ.get("REDIS_HOST"), port=os.environ.get("REDIS_PORT"))
     try:
         yield redis_conn
+    except ConnectionError as e:
+        logger.error(f"Unable to connect to Redis: {e}")
+        raise e
+    finally:
+        await redis_conn.aclose()
+
+@asynccontextmanager
+async def redis_conn_manager():
+    """Connection manager for inside background tasks"""
+    redis_conn = redis.Redis(host=os.environ.get("REDIS_HOST"), port=os.environ.get("REDIS_PORT"))
+    try:
+        yield redis_conn
+    except ConnectionError as e:
+        logger.error(f"Unable to connect to Redis: {e}")
+        raise e
     finally:
         await redis_conn.aclose()
 
@@ -30,10 +60,95 @@ def get_bybit_session() -> HTTP:
     finally: 
         # session has no close method 
         pass
+
+
+async def fetch_exchange_ws_stream( stream: str = "publicTrade.BTCUSDT") -> None:
+    """Connect to the websocket stream of exchange and save to a Redis stream for further use
     
+    Args:
+        stream:     name of the stream channel to connect. reference in https://bybit-exchange.github.io/docs/v5/ws/connect """
+    
+    async with redis_conn_manager() as redis_db:
+        # connect to the bybit ws stream
+        uri = "wss://stream.bybit.com/v5/public/linear"
+        # uri = "wss://stream-testnet.bybit.com/v5/public/linear"
+        try:
+            async with websockets.connect(uri) as websocket_exchange:
+                # suscribe to a stream/channel
+                payload = {
+                        "op": "subscribe",
+                        "args": [
+                            stream,
+                        ]
+                    }
+                await websocket_exchange.send(json.dumps(payload))
+                
+                # read response of the subscription to the exchange
+                init_msg = await websocket_exchange.recv()
+                logger.debug(init_msg)
 
-app = FastAPI()
+                stream_name = stream.replace(".",":")
+                try:
+                    info = await redis_db.xinfo_stream(stream_name)
+                    last_id  = info['last-entry'][0].decode("utf-8")
+                except ResponseError as e:
+                    logger.debug(f"key {stream_name} not found, start at zero ({e})")
+                    last_id = "0-0"
 
+                # once connected to the exchanges trade stream, fetch the messages and do something with it
+                ts_prev = datetime.now().timestamp()
+                while True:
+                    # send a ping one every minute or so (https://websockets.readthedocs.io/en/stable/reference/asyncio/client.html#websockets.client.WebSocketClientProtocol.ping)
+                    ts_now = datetime.now().timestamp()
+                    if ts_now - ts_prev >= 60:
+                        await websocket_exchange.ping()
+                        ts_prev = ts_now
+                    msg_q = websocket_exchange.messages
+                    msg = await websocket_exchange.recv()
+                    obj = json.loads(msg)
+                    logger.debug(f"{stream_name} queue length: {len(msg_q)}. data length:{len(obj['data'])}")
+
+                    # send obj['data'] to redis
+                    for data in obj['data']:
+                        
+                        if int(data['T']) > int(last_id.split("-")[0]):
+                            new_id = f"{data['T']}-{0}"
+                        elif int(data['T']) == int(last_id.split("-")[0]):
+                            id = int(last_id.split("-")[1]) + 1
+                            new_id = f"{data['T']}-{id}"
+                        else:
+                            pass
+                        data['BT'] = int(data['BT'])  # redis doesn't accept booleans
+                        await redis_db.xadd(name=stream_name, fields=data, id=new_id, )
+                        last_id = new_id
+
+        except  websockets.exceptions.ConnectionClosedOK as e:
+            logger.info(f"connection closed! {e}")
+        except Exception as e:
+            logger.error(e)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    for symbol in get_symbols():
+        stream = f"publicTrade.{symbol['symbol']}"
+        asyncio.create_task(fetch_exchange_ws_stream(stream))
+
+    # asyncio.create_task(fetch_exchange_ws_stream("publicTrade.ETHUSDT"))
+    # asyncio.create_task(fetch_exchange_ws_stream("publicTrade.BTCSDT"))
+    # asyncio.create_task(fetch_exchange_ws_stream("publicTrade.SOLUSDT"))
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/start_trades")
+# def start_trades(background_tasks: BackgroundTasks, stream:str = "publicTrade.ETHUSDT",  redis_db: redis.Redis = Depends(get_redis_conn)):
+def start_trades(background_tasks: BackgroundTasks, stream:str = "publicTrade.ETHUSDT"):
+
+    background_tasks.add_task(fetch_exchange_ws_stream, stream=stream,)# redis_db=redis_db )
+    return {"message": "start with fetching trade info in the background"}
 
 @app.get("/")
 def read_root():
@@ -41,7 +156,7 @@ def read_root():
 
 
 @app.get("/api/test_redis")
-async def test_redis(redis_db: redis.Redis = Depends(get_redis_conn)):
+async def test_redis(redis_db: Annotated[redis.Redis, Depends(get_redis_conn)]):
     await redis_db.set("test", json.dumps({"hoi": "doei"}))
     output = await redis_db.get("test")
     return output
