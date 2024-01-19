d51793bda08204fe2bb500f47488166c161f7238
HungryArie <arnoudzwakenberg@gmail.com>
2024-01-19 16:22:35 +0000

    made errorhandling simpler - set ruff linelength to 150 - `ws.websocket_trades`   - bugfix when closing connection OK.   - and closing with error - `subscribe.check_subscription_call`   - de-indeded some try except clauses   - removed commented code

--- api/app/routers/ws.py
+++ api/app/routers/ws.py
@@ -1,18 +1,22 @@
 import os
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from fastapi.websockets import WebSocketState
+from websockets import ConnectionClosedError, ConnectionClosedOK
 from app.db.consumer.trades import trades_consumer
 from app.logger import streaming_logger
 from app.websocket.models import TradesStreamModel
 from app.websocket.subscribe import check_subscription_call
 
-router = APIRouter(prefix="")
-logger = streaming_logger(__name__, os.getenv("API_LOGGING_LEVEL", "ERROR"))
 
-  
+router = APIRouter(prefix="")
+
+logger = streaming_logger(__name__, os.getenv("API_LOGGING_LEVEL", "ERROR"))
+logger.setLevel(10)  # set to debug
+
+
 @router.websocket("/trades")
 async def websocket_trades(websocket: WebSocket):
-    """provides a websocket endpoint for realtime trades from the exchanges 
+    """provides a websocket endpoint for realtime trades from the exchanges
     that are cached, so also old trades can be fetched"""
     # based on https://fastapi.tiangolo.com/advanced/websockets/
 
@@ -20,25 +24,28 @@ async def websocket_trades(websocket: WebSocket):
     await websocket.accept()
     try:
         subscription = await check_subscription_call(websocket, TradesStreamModel)
+
         # subscribe to the redis stream
         async for trade in trades_consumer(subscription["stream"], subscription["timestamp"]):
+            # TODO handle Task exception was never retrieved
+            # api  | future: <Task finished name='Task-17' coro=<<async_generator_athrow without __name__>()> exception=RuntimeError('async generator ignored GeneratorExit')>
+            # api  | RuntimeError: async generator ignored GeneratorExit
             await websocket.send_text(f"Message data was: {trade}")
 
-    except (WebSocketDisconnect) as e: #, ConnectionClosed, ConnectionClosedOK) as e:
+    except (WebSocketDisconnect, ConnectionClosedOK) as e:
         logger.info(f"connection closed: {e}")
-        # await websocket.close()  NOT needed because we are already disconnected
+        websocket.client_state = WebSocketState.DISCONNECTED
     finally:
         logger.debug(f"App state:{websocket.application_state}, client state: {websocket.client_state}")
         if websocket.client_state == WebSocketState.CONNECTED and websocket.application_state == WebSocketState.CONNECTED:
-            try: 
+            try:
                 await websocket.send_text(f"goodbye... connecion will be terminated{0}")
                 await websocket.close()
                 logger.info("finished, connection is closed")
-            except Exception as e:
+            except (ConnectionClosedError, ConnectionClosedOK) as e:
                 logger.info(f"could not disconnect, client already disconnected, error: {e}")
         elif websocket.client_state == WebSocketState.DISCONNECTED:
             logger.info("finished, client is already disconnected")
         else:
             logger.info("finished, connection is closed")
-        pass  # TODO CHECK with debugging! is connection closed or should i close it here?
-        
+        pass
